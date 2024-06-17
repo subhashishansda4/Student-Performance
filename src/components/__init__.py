ac9cd65ae3062ad81561363a1b580abe9ee5bb91
Subhashis Hansda <subhashishansda2@gmail.com>
2024-06-17 18:15:27 +0530

    eda

--- src/components/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 19 21:51:50 2024
-
-@author: Rahul
-"""
-
--- src/logger.py
+++ src/logger.py
@@ -13,3 +13,14 @@ logging.basicConfig(
     format = '[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s',
     level = logging.INFO
 )
+
+'''
+x = 1000
+print([n for n in range(2,x+1) if all(n%i != 0 for i in range(2,n))])
+
+n = 6
+print('Prime' if n>1 and all(n%i != 0 for i in range(2, int(n**0.5)+1)) else 'Not Prime')
+
+def prime(n): return not any(n%i == 0 for i in range(2,n))
+prime(6)
+'''
